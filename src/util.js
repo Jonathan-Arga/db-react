73e27a11cc7815748e1f79e9dab1bc6dfd5e81ad
Michael Hazan <michaelhazan@hilma.tech>
2024-08-05 15:31:07 +0300

    useFetch + util.js (#8)
    * util init + useFetch v0.1
    
    * v0.0.2: added failed incase of failure in fetch

---
+++ src/util.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const MAIN_URL = 'https://localhost:3000/';
+function useFetch(path) {
+	const [data, setData] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const [failed, setFailed] = useState(false);
+	try {
+		setIsLoading(true);
+		setFailed(false);
+		fetch(`${MAIN_URL}/${path}`)
+			.then((res) => {
+				if (!res.ok) setFailed(true);
+				return res.json();
+			})
+			.then((data) => {
+				setData(data);
+				setIsLoading(false);
+			})
+			.catch((err) => (err ? setFailed(true) : false));
+	} catch (err) {
+		console.error(err);
+		setFailed(true);
+	}
+	return { isLoading, failed, data };
+}
+
+export { useFetch };
