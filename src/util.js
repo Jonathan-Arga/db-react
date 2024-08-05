d161f01e43882cbdca1ad8bb69df0902834c5a7e
Michael Hazan <michaelhazan@hilma.tech>
2024-08-05 15:52:49 +0300

    util.js changes (#9)
    * useFetch v0.0.3 allows post method if bodyData is given
    
    * changed bodyData => options

--- src/util.js
+++ src/util.js
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 
 const MAIN_URL = 'https://localhost:3000/';
-function useFetch(path) {
+
+function useFetch(path, options = { method: 'GET', data: {} }) {
 	const [data, setData] = useState();
 	const [isLoading, setIsLoading] = useState(true);
 	const [failed, setFailed] = useState(false);
+
 	try {
 		setIsLoading(true);
 		setFailed(false);
-		fetch(`${MAIN_URL}/${path}`)
+		fetch(`${MAIN_URL}/${path}`, {
+			method: options.method,
+			body: options.data,
+		})
 			.then((res) => {
 				if (!res.ok) setFailed(true);
 				return res.json();
