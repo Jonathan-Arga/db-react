e4998f258d69b90723c63fc4ad5bda5f67a1195a
michaelh-hilma <michaelhazan@hilma.tech>
2024-08-05 17:09:49 +0300

    more stuff idk

--- src/pages/Signup/components/RegisterForm.jsx
+++ src/pages/Signup/components/RegisterForm.jsx
@@ -1,20 +1,31 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useFetch } from '../../../util';
+import styles from './RegisterForm.module.css';
 
 export default function RegisterForm() {
-	const fetch = useFetch('/');
+	//const fetch = useFetch();
 	/**
-	 *
 	 * @param {React.FormEvent} e
 	 */
 	const submitHandler = (e) => {
 		e.preventDefault();
 		fetch.setRequest('users');
 	};
+	console.log('rendered');
 	return (
-		<form onSubmit={(e) => submitHandler(e)}>
-			<button>button</button>
-			{JSON.stringify(fetch)}
+		<form className={styles.signupForm}>
+			<input name='email' type='email' placeholder='Email here' />
+			<input
+				name='password'
+				type='password'
+				placeholder='Password here'
+			/>
+			<input
+				name='same-password'
+				type='password'
+				placeholder='Repeat password here'
+			/>
+			<button value='Submit' onSubmit={submitHandler} />
 		</form>
 	);
 }
