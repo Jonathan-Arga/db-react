0bc9712b1d8271623fb8c503037db570d32752de
michaelh-hilma <michaelhazan@hilma.tech>
2024-08-05 15:41:30 +0300

    useFetch v0.0.3 allows post method if bodyData is given

--- src/util.js
+++ src/util.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 
 const MAIN_URL = 'https://localhost:3000/';
-function useFetch(path) {
+
+function useFetch(path, bodyData = undefined) {
 	const [data, setData] = useState();
 	const [isLoading, setIsLoading] = useState(true);
 	const [failed, setFailed] = useState(false);
+
+	let sendMethod = 'GET';
+
+	if (bodyData) sendMethod = 'POST';
+
 	try {
 		setIsLoading(true);
 		setFailed(false);
-		fetch(`${MAIN_URL}/${path}`)
+		fetch(`${MAIN_URL}/${path}`, { method: sendMethod, body: bodyData })
 			.then((res) => {
 				if (!res.ok) setFailed(true);
 				return res.json();
