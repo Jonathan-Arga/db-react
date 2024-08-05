d43edd250ff535a64d01142db2174905b7952432
Yonatan <jonathanargaman@hilma.tech>
2024-08-05 13:32:01 +0300

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [d, setd] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	async function getUsers() {
+		const res = await fetch("http://localhost:3000/users");
+		const data = await res.json();
+		setd(data);
+	}
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/" element={<Layout />}>
+					<Route path="home" element={<Home />} />
+					<Route path="todos" element={<Todos />} />
+					<Route path="posts" element={<PostLayout />}>
+						<Route index element={<Posts />} />
+						<Route path=":postid" element={<PostItem />} />
+					</Route>
+					<Route path="albums" element={<AlbumLayout />}>
+						<Route index element={<Albums />} />
+						<Route path=":photoid" element={<AlbumItem />} />
+					</Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
