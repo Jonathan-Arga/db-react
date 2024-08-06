31ee86e3073022fa7893470e4ac535c0642d640d
michaelh-hilma <michaelhazan@hilma.tech>
2024-08-06 13:06:43 +0300

    Notifications init

--- src/App.jsx
+++ src/App.jsx
@@ -15,40 +15,54 @@ import MainLayout from './Layout/MainLayout/MainLayout';
 
 import './App.css';
 import { createContext, useState } from 'react';
+import Notifications from './components/Notifications/Notifications';
 
 export const MAIN_URL = 'http://localhost:3000/';
 
 export const DeletingPostsContext = createContext(null);
+export const NotificationsListContext = createContext(null);
 
 function App() {
 	const [DeletingPostsContextState, SetDeletingPostsContextState] =
 		useState(false);
+	const [NotificationsListContextState, SetNotificationsListContextState] =
+		useState([]);
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/" element={<MainLayout />}>
-					<Route path="home" element={<Home />} />
-					<Route path="todos" element={<Todos />} />
-					<DeletingPostsContext.Provider
-						value={[
-							DeletingPostsContextState,
-							SetDeletingPostsContextState,
-						]}
-					>
-						<Route path="posts" element={<PostsLayout />}>
-							<Route index element={<Posts />} />
-							<Route path=":postid" element={<PostItem />} />
+		<>
+			<NotificationsListContext.Provider
+				value={[
+					NotificationsListContextState,
+					SetNotificationsListContextState,
+				]}
+			>
+				<Notifications />
+			</NotificationsListContext.Provider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/" element={<MainLayout />}>
+						<Route path="home" element={<Home />} />
+						<Route path="todos" element={<Todos />} />
+						<DeletingPostsContext.Provider
+							value={[
+								DeletingPostsContextState,
+								SetDeletingPostsContextState,
+							]}
+						>
+							<Route path="posts" element={<PostsLayout />}>
+								<Route index element={<Posts />} />
+								<Route path=":postid" element={<PostItem />} />
+							</Route>
+						</DeletingPostsContext.Provider>
+						<Route path="albums" element={<AlbumsLayout />}>
+							<Route index element={<Albums />} />
+							<Route path=":photoid" element={<AlbumItem />} />
 						</Route>
-					</DeletingPostsContext.Provider>
-					<Route path="albums" element={<AlbumsLayout />}>
-						<Route index element={<Albums />} />
-						<Route path=":photoid" element={<AlbumItem />} />
 					</Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
---
+++ src/components/Notifications/Notifications.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { NotificationsListContext } from '../../App';
+import NotificationItem from './components/NotificationItem';
+import styles from './Notifications.module.css';
+
+/**
+ * Add a notification to show.
+ * @param {'error' | 'success' | 'warn'} type
+ * @param {string} message
+ */
+
+export function AddNotification(type, message) {
+	const [, SetNotificationsListContextState] = useContext(
+		NotificationsListContext
+	);
+	SetNotificationsListContextState((prevNotifications) => [
+		...prevNotifications,
+		<li key={type + message}>
+			<NotificationItem message={message} type={type} />
+		</li>,
+	]);
+}
+
+export default function Notifications() {
+	const [NotificationsListContextState] = useContext(
+		NotificationsListContext
+	);
+	return <ul className="">{NotificationsListContextState.map()}</ul>;
+}
