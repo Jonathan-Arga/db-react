5bf9b06cf8c74bec713bb0a9b019c791468d7c0e
Yonatan <jonathanargaman@hilma.tech>
2024-08-06 15:19:51 +0300

    deleted glitchy notifs

--- src/App.jsx
+++ src/App.jsx
@@ -14,44 +14,22 @@ import PostsLayout from "./Layout/PostsLayout/PostsLayout";
 import MainLayout from "./Layout/MainLayout/MainLayout";
 
 import "./App.css";
-import { createContext, useState } from "react";
-import Notifications from "./components/Notifications/Notifications";
-
 export const MAIN_URL = "http://localhost:3000/";
 
-export const NotificationsListContext = createContext(null);
-
 function App() {
-	const [NotificationsListContextState, SetNotificationsListContextState] =
-		useState([]);
 	return (
-		<>
-			<NotificationsListContext.Provider
-				value={[
-					NotificationsListContextState,
-					SetNotificationsListContextState,
-				]}
-			>
-				<Notifications />
-			</NotificationsListContext.Provider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/" element={<MainLayout />}>
-						<Route path="home" element={<Home />} />
-						<Route path="todos" element={<Todos />} />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/" element={<MainLayout />}>
+					<Route path="home" element={<Home />} />
+					<Route path="todos" element={<Todos />} />
 
-						<Route path="posts" element={<PostsLayout />}>
-							<Route index element={<Posts />} />
-							<Route path=":postid" element={<PostItem />} />
-						</Route>
-						<Route path="albums" element={<AlbumsLayout />}>
-							<Route index element={<Albums />} />
-							<Route path=":photoid" element={<AlbumItem />} />
-						</Route>
+					<Route path="posts" element={<PostsLayout />}>
+						<Route index element={<Posts />} />
+						<Route path=":postid" element={<PostItem />} />
 					</Route>
-
 					<Route path="albums" element={<AlbumsLayout />}>
 						<Route index element={<Albums />} />
 						<Route
@@ -59,9 +37,9 @@ function App() {
 							element={<AlbumPage />}
 						/>
 					</Route>
-				</Routes>
-			</BrowserRouter>
-		</>
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
