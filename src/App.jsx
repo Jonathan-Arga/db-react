00915ca0294562b9022232186c0b8fef78371c18
michaelh-hilma <michaelhazan@hilma.tech>
2024-08-06 12:32:55 +0300

    DeletingPostsContext + basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Login from "./pages/Login/Login";
-import Register from "./pages/Signup/Register";
-import Home from "./pages/Home/Home";
-import Todos from "./pages/Todos/Todos";
-import Posts from "./pages/Posts/Posts";
-import PostItem from "./pages/Posts/components/PostItem";
-import Albums from "./pages/Albums/Albums";
-import AlbumItem from "./pages/Albums/components/AlbumItem";
+import Login from './pages/Login/Login';
+import Register from './pages/Signup/Register';
+import Home from './pages/Home/Home';
+import Todos from './pages/Todos/Todos';
+import Posts from './pages/Posts/Posts';
+import PostItem from './pages/Posts/components/PostItem';
+import Albums from './pages/Albums/Albums';
+import AlbumItem from './pages/Albums/components/AlbumItem';
 
-import AlbumsLayout from "./Layout/AlbumsLayout/AlbumsLayout";
-import PostsLayout from "./Layout/PostsLayout/PostsLayout";
-import MainLayout from "./Layout/MainLayout/MainLayout";
+import AlbumsLayout from './Layout/AlbumsLayout/AlbumsLayout';
+import PostsLayout from './Layout/PostsLayout/PostsLayout';
+import MainLayout from './Layout/MainLayout/MainLayout';
 
-import "./App.css";
+import './App.css';
+import { createContext, useState } from 'react';
+
+export const MAIN_URL = 'http://localhost:3000/';
+
+export const DeletingPostsContext = createContext(null);
 
 function App() {
+	const [DeletingPostsContextState, SetDeletingPostsContextState] =
+		useState(false);
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -24,10 +31,17 @@ function App() {
 				<Route path="/" element={<MainLayout />}>
 					<Route path="home" element={<Home />} />
 					<Route path="todos" element={<Todos />} />
-					<Route path="posts" element={<PostsLayout />}>
-						<Route index element={<Posts />} />
-						<Route path=":postid" element={<PostItem />} />
-					</Route>
+					<DeletingPostsContext.Provider
+						value={[
+							DeletingPostsContextState,
+							SetDeletingPostsContextState,
+						]}
+					>
+						<Route path="posts" element={<PostsLayout />}>
+							<Route index element={<Posts />} />
+							<Route path=":postid" element={<PostItem />} />
+						</Route>
+					</DeletingPostsContext.Provider>
 					<Route path="albums" element={<AlbumsLayout />}>
 						<Route index element={<Albums />} />
 						<Route path=":photoid" element={<AlbumItem />} />
--- src/Layout/PostsLayout/PostsLayout.jsx
+++ src/Layout/PostsLayout/PostsLayout.jsx
@@ -1,9 +1,50 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, redirect, useLocation } from 'react-router-dom';
+import { DeletingPostsContext, MAIN_URL } from '../../App';
+import { useContext } from 'react';
 
 export default function PostsLayout() {
+	const location = useLocation();
+	const [isDeletingPosts, setIsDeletingPosts] =
+		useContext(DeletingPostsContext);
+	const currentPage = location.pathname.substring(
+		location.pathname.lastIndexOf('/') + 1
+	);
+	/**
+	 * @param {MouseEvent} e
+	 */
+	const HandleNewPostClick = () => {};
+	/**
+	 * @param {MouseEvent} e
+	 */
+	const HandleDeletePostClick = () => {
+		if (currentPage != 'posts')
+			// delete currentpage cause its a post
+			return fetch(MAIN_URL + `posts/${currentPage}`, {
+				method: 'DELETE',
+			}).then(() => redirect('/posts'));
+		setIsDeletingPosts((prevIsDeletingPosts) => !prevIsDeletingPosts);
+	};
 	return (
 		<>
-			<h1>Posts layout</h1>
+			<div className="postsLayoutHeader">
+				<div className="title">Posts</div>
+				{currentPage === 'posts' ? (
+					<button
+						className="NewPostButton"
+						onClick={HandleNewPostClick}
+					>
+						&#x2B;
+					</button>
+				) : (
+					false
+				)}
+				<button
+					className="DeletePostButton"
+					onClick={HandleDeletePostClick}
+				>
+					&#128465;
+				</button>
+			</div>
 			<Outlet />
 		</>
 	);
