3d74a796b66d17c92f0dd18d7258436b36126cc0
michaelh-hilma <michaelhazan@hilma.tech>
2024-08-06 09:16:12 +0300

    LoginForm css, RegisterForm start and css

--- src/pages/Signup/components/RegisterForm.jsx
+++ src/pages/Signup/components/RegisterForm.jsx
@@ -1,31 +1,54 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useRef } from 'react';
 import { useFetch } from '../../../util';
 import styles from './RegisterForm.module.css';
 
 export default function RegisterForm() {
-	//const fetch = useFetch();
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const usernameRef = useRef();
+	const passwordRef = useRef();
+	const repeatPasswordRef = useRef();
+
+	const users = useFetch();
+	const postData = useFetch();
+
 	/**
 	 * @param {React.FormEvent} e
 	 */
 	const submitHandler = (e) => {
 		e.preventDefault();
-		fetch.setRequest('users');
 	};
-	console.log('rendered');
+	useEffect(() => {
+		postData.setRequest('users', {
+			method: 'POST',
+			body: {
+				'id': users.data.length,
+				'name': nameRef.current.value,
+				'username': usernameRef.current.value,
+				'email': emailRef.current.value,
+				'website': passwordRef.current.value,
+			},
+		});
+	});
 	return (
-		<form className={styles.signupForm}>
-			<input name='email' type='email' placeholder='Email here' />
+		<form className={styles.signupForm} onSubmit={submitHandler}>
+			<input ref={nameRef} type='text' placeholder='Full Name' />
+			<input ref={usernameRef} type='text' placeholder='Username' />
+			<input ref={emailRef} type='email' placeholder='Email here' />
 			<input
+				ref={passwordRef}
 				name='password'
 				type='password'
 				placeholder='Password here'
 			/>
 			<input
+				ref={repeatPasswordRef}
 				name='same-password'
 				type='password'
 				placeholder='Repeat password here'
 			/>
-			<button value='Submit' onSubmit={submitHandler} />
+			<button onSubmit={submitHandler}>Sign Up</button>
 		</form>
 	);
 }
