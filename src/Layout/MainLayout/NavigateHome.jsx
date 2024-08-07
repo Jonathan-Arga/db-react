2dd4a2413afc06f096dceb9bf86f5552ce8cb15a
Yonatan <jonathanargaman@hilma.tech>
2024-08-07 09:48:11 +0300

    added rerouting from path "/"

---
+++ src/Layout/MainLayout/NavigateHome.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { checkLoggedIn } from "../../util";
+import { useEffect } from "react";
+
+export default function NavigateHome() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (checkLoggedIn()) {
+			navigate("home");
+		} else {
+			navigate("login");
+		}
+	}, []);
+}
